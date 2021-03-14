5111c64329465539fee47d4bfb3d925bf5e8b0ed
unknown <mohsinsait23@gmail.com>
2021-03-14 17:18:10 +0530

    added category model

--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, Article, AddBlog, UpdatePostView, DeletePostView
+from .views import HomeView, Article, AddBlog, UpdatePostView, DeletePostView, AddCategory
 # from . import views
 
 urlpatterns = [
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', Article.as_view(), name="article-detail"),
     path('add_post/', AddBlog.as_view(), name="add_post"),
+    path('add_category/', AddCategory.as_view(), name="add_category"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
 ]
